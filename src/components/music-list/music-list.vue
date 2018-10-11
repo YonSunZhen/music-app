d9fd1ba79b38a080b22fa2ce88d10a1d0b6f3bb6
YonSunZhen <1315976441@qq.com>
2018-10-11 16:44:38 +0800

    完成singer-detail

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,16 +1,117 @@
 <template>
   <div class="music-list">
     <div class="back">
-      <i class="icon-back"></i>
+      <router-link class="icon-back" to="/singer"></router-link>
     </div>
-    <h1 class="title"></h1>
-    <div class="bg-image">
+    <h1 class="title">{{title}}</h1>
+    <div class="bg-image" :style="bgStyle" ref="bgmusic">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playButton">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter"></div>
     </div>
+    <!-- 设置一个偏移层 -->
+    <div class="bg-layer" ref="layer"></div>
+    <scroll @scroll="scroll" :probe-type="probeType" :listen-scroll="listenScroll" :data="songs" class="list" ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+      <div v-show="!songs.length" class="loading-container">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+  import SongList from 'base/song-list/song-list'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+
+  const RESERVED_HEIGHT = 40;
+
+  export default {
+    // created() {
+    //   console.log(this.title);
+    // },
+    props: {
+      bgImage: {
+        type: String,
+        default: ''
+      },
+      songs: {
+        type: Array,
+        default: []
+      },
+      title: {
+        type: String,
+        default: ''
+      }
+    },
+    data: function() {
+      return {
+        scrollY: 0
+      }
+    },
+    computed: {
+      bgStyle() {
+        return `background-image:url(${this.bgImage})`;
+      }
+    },
+    created() {
+      this.probeType = 3
+      this.listenScroll = true
+    },
+    mounted() {
+      this.imageHeight = this.$refs.bgmusic.clientHeight;
+      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT;
+      this.$refs.list.$el.style.top = `${this.imageHeight}px`;
+    },
+    methods: {
+      scroll(pos) {
+        this.scrollY = pos.y;
+      }
+    },
+    watch: {
+      scrollY(newY) {
+        // 滚动的最大距离就是图片的高度，两个都是负数
+        let translateY = Math.max(this.minTranslateY, newY);
+        let zIndex = 0;
+        let scale = 1
+        this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`;
+        this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`;
+        const percent = Math.abs(newY / this.imageHeight);
+        // 当往下拉的时候
+        if (newY > 0) {
+          // 这里表示头像放大的倍数
+          scale = 1 + percent;
+          zIndex = 10;
+        }
+        // 当到达顶部的时候
+        if (newY < this.minTranslateY) {
+          zIndex = 10;
+          this.$refs.bgmusic.style.paddingTop = 0;
+          this.$refs.bgmusic.style.height = `${RESERVED_HEIGHT}px`;
+          this.$refs.playButton.style.display = 'none';
+        } else {
+          this.$refs.bgmusic.style.paddingTop = '70%';
+          this.$refs.bgmusic.style.height = 0;
+          this.$refs.playButton.style.display = '';
+        }
+        this.$refs.bgmusic.style.zIndex = zIndex;
+        this.$refs.bgmusic.style['transform'] = `scale(${scale})`;
+        this.$refs.bgmusic.style['webkitTransform'] = `scale(${scale})`;
+      }
+    },
+    components: {
+      SongList,
+      Scroll,
+      Loading
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
@@ -88,11 +189,14 @@
       position: relative
       height: 100%
       background: $color-background
+      transform: translate3d(0,30px,0)
+      -webkit-transform: translate3d(0,30px,0)
     .list
       position: fixed
       top: 0
       bottom: 0
       width: 100%
+      // overflow hidden
       background: $color-background
       .song-list-wrapper
         padding: 20px 30px
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slider">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -9,6 +9,7 @@
   import {getDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import musicList from 'components/music-list/music-list'
 
   export default {
     data: function() {
@@ -17,6 +18,12 @@
       }
     },
     computed: {
+      title() {
+        return this.singer.name;
+      },
+      bgImage() {
+        return this.singer.avatar;
+      },
       ...mapGetters([
         'singer'
       ])
@@ -32,9 +39,9 @@
         }
         getDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
-            // console.log(res.data.list);
+            console.log(res.data.list);
             this.songs = this._normalizeSongs(res.data.list);
-            console.log(this.songs);
+            // console.log(this.songs);
           }
         })
       },
@@ -48,6 +55,9 @@
         })
         return ret;
       }
+    },
+    components: {
+      musicList
     }
   }
 </script>
